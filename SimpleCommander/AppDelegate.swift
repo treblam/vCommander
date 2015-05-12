c1b9c7a38e5ec12ac8710c15647cb4dd40a22e98
Jamie <treblam@gmail.com>
2015-05-12 22:22:59 +0800

    Initial Commit

---
+++ SimpleCommander/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SimpleCommander
+//
+//  Created by Jamie on 15/5/12.
+//  Copyright (c) 2015年 Jamie. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
